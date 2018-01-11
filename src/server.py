bc6b848345bc116b0560851063999986ebcf0aee
Paweł Kupidura <pkupidura@evojam.com>
2018-01-11 11:30:34 +0100

    Added pipenv.

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import flask as flask
 from flask import Flask
 from flask import Response
 from flask import request
@@ -10,22 +11,25 @@ app = Flask(__name__)
 
 login = os.environ['JIRA_USER']
 passw = os.environ['JIRA_PASS']
-credentials=(login, passw)
-api='https://evojam.atlassian.net/rest/api/latest'
-timesheets_api='https://evojam.atlassian.net/rest/tempo-timesheets/3'
+api = 'https://evojam.atlassian.net/rest/api/latest'
+timesheets_api = 'https://evojam.atlassian.net/rest/tempo-timesheets/3'
+
 
 def reversed_date(date):
     splitted = date.split('-')
     splitted.reverse()
     return '-'.join(splitted)
 
+
 @app.route("/api")
 def hello():
     return "Hello World!"
 
+
 @app.route("/health")
 def health():
     return Response("{'Status':'working'}", status=200, mimetype='application/json')
+
 
 @app.route("/worklogs")
 def worklogs():
@@ -35,17 +39,20 @@ def worklogs():
     )
     return response.text, response.status_code
 
+
 class Worklog:
     def __init__(self, name, minutes, date):
         self.name = name
         self.minutes = minutes
         self.date = date
 
+
 def parse_worklog(record):
     name = record['author']['displayName']
     minutes = int(int(record['billedSeconds'])/60)
     date = reversed_date(record['dateCreated'][0:10])
     return Worklog(name, minutes, date)
+
 
 @app.route("/api/dashboard")
 def dashboard():
@@ -54,8 +61,7 @@ def dashboard():
 
     url = timesheets_api + '/worklogs/?dateFrom=' + date_from + '&dateTo=' + date_to
 
-    response = requests.get(url, auth=credentials)
-
+    response = requests.get(url, auth=(login, passw))
     worklogs = [parse_worklog(record) for record in response.json()]
 
     names = list(set([w.name for w in worklogs]))
@@ -63,7 +69,7 @@ def dashboard():
     d = dict([(name, list()) for name in names])
 
     for w in worklogs:
-        d[w.name].append({ 'date': w.date, 'minutes': w.minutes })
+        d[w.name].append({'date': w.date, 'minutes': w.minutes})
 
     result = list()
 
